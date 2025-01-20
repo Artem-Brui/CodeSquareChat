99ca84f29b189694429f1474e5a3aaea559429a2
KathrinKath <kathrin.peled@dci-student.org>
2025-01-20 14:07:06 +0100

    Terms and Conditions page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import FAQPage from "./components/log-in/faq/FAQPage";
+import TermsAndConditions from "./components/log-in/term-conditions/TermsAndConditions";
+// import React, { Component } from "react";
 
 function App() {
   return (
     <div>
       <FAQPage />
+      <TermsAndConditions />
     </div>
   );
 }
