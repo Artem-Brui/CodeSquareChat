292973dad21f75f5725ac02e46fd02e40e6105cf
FabriceMru <jean.nadal@dci-student.org>
2025-01-28 09:42:53 +0100

    push changes fabrice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,44 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import FAQPage from "./pages/FAQPage";
-import TermsAndConditions from "./pages/TermsAndConditions";
-import WelcomePage from "./pages/WelcomePage";
-import MainPageChat from "./pages/MainPageChat.jsx";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FAQPage from './pages/FAQPage';
+import TermsAndConditions from './pages/TermsAndConditions';
+import WelcomePage from './pages/WelcomePage';
+import MainPageChat from './pages/MainPageChat.jsx';
 // import BurgerMenu from './components/layout/BurgerMenu';
 // import Header from './components/layout/Header';
 // import ChatCategories from './components/chat/ChatCategories';
 // import AddRoom from './components/pages/AddRoom';
 // import ChatTextBox from './components/chat/ChatTextBox';
-import UserRegistrationFormPage from "./pages/UserRegistrationPage";
+import UserRegistrationFormPage from './pages/UserRegistrationPage';
 // import UserProfilePage from './components/users/Profile/UserProfilePage';
 // import UserSettingsPage from './components/users/UserSettings/UserSettingsPage';
 
-import SignUpUserInfoForm from "./components/users/registration/SignUpUserInfoForm";
+import SignUpUserInfoForm from './components/users/registration/SignUpUserInfoForm';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/register" element={<UserRegistrationFormPage />} />
-        <Route path="/faq" element={<FAQPage />} />
-        <Route path="/t&cs" element={<TermsAndConditions />} />
-        <Route path="/mainpagechat" element={<MainPageChat />} />
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<WelcomePage />} />
+                <Route
+                    path="/register"
+                    element={<UserRegistrationFormPage />}
+                />
+                <Route path="/faq" element={<FAQPage />} />
+                <Route path="/t&cs" element={<TermsAndConditions />} />
+                <Route path="/mainpagechat" element={<MainPageChat />} />
 
-        {/* check validation of birthdate */}
-        <Route path="/signup" element={<SignUpUserInfoForm />} />
+                {/* check validation of birthdate */}
+                <Route path="/signup" element={<SignUpUserInfoForm />} />
 
-        {/* <Route path="/chat-categories" element={<ChatCategories />} />
+                {/* <Route path="/chat-categories" element={<ChatCategories />} />
         <Route path="/add-room" element={<AddRoom />} />
         <Route path="/chat-text-box" element={<ChatTextBox />} />
        
         <Route path="/user-profile-page" element={<UserProfilePage />} />
         <Route path="/user-settings-page" element={<UserSettingsPage />} /> */}
-      </Routes>
-    </Router>
-  );
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
