ab6ca97fef763f499da1c22d26504c793b63215f
Artem-Brui <artembryj@gmail.com>
2025-01-29 11:47:09 +0100

    add api for rooms from DB in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,39 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import FAQPage from './pages/FAQPage';
-import TermsAndConditions from './pages/TermsAndConditions';
-import WelcomePage from './pages/WelcomePage';
-import MainPageChat from './pages/MainPageChat';
-import ChatCategories from './components/chat/ChatCategories';
-import AddRoom from './pages/RoomsList';
-import ChatTextBox from './components/chat/ChatTextBox';
-import UserRegistrationFormPage from './pages/UserRegistrationPage';
-import UserProfilePage from './pages/UserProfilePage';
-import UserSettingsPage from './components/users/UserSettings/UserSettingsPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FAQPage from "./pages/FAQPage";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import WelcomePage from "./pages/WelcomePage";
+import MainPageChat from "./pages/MainPageChat";
+import ChatCategories from "./components/chat/ChatCategories";
+import AddRoom from "./pages/RoomsList";
+import ChatTextBox from "./components/chat/ChatTextBox";
+import UserRegistrationFormPage from "./pages/UserRegistrationPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import UserSettingsPage from "./components/users/UserSettings/UserSettingsPage";
 // import BurgerMenu from './components/layout/BurgerMenu';
 // import Header from './components/layout/Header';
 // import ChatTextBox from './components/chat/ChatTextBox';
 
-import SignUpUserInfoForm from './components/users/registration/SignUpUserInfoForm';
-import Dashboard from './pages/Dashboard';
-import RoomPage from './pages/RoomPage';
-import BurgerMenu from './components/layout/BurgerMenu';
-import { roomsList } from './services/database.js';
+import SignUpUserInfoForm from "./components/users/registration/SignUpUserInfoForm";
+import Dashboard from "./pages/Dashboard";
+import RoomPage from "./pages/RoomPage";
+import BurgerMenu from "./components/layout/BurgerMenu";
+// import { roomsList } from './services/database.js';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [roomsList, setRoomsList] = useState([]);
+
+  useEffect(() => {
+    const getRooms = async () => {
+      const RoomsBDResponse = await fetch("http://localhost:5007/rooms");
+      const roomsData = await RoomsBDResponse.json();
+
+      setRoomsList(await roomsData);
+    };
+
+    getRooms();
+  }, []);
+
   return (
     <Router>
       <Routes>
