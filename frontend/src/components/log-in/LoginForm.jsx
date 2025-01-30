95d82a37188fa7718390184c9278450e51979ec7
FabriceMru <jean.nadal@dci-student.org>
2025-01-30 14:28:56 +0100

    loading screen and rotating icon added

--- frontend/src/components/log-in/LoginForm.jsx
+++ frontend/src/components/log-in/LoginForm.jsx
@@ -1,71 +1,81 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import api from '../../../config/api';
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { LoadingSpinner } from "./LoadingSpinner.jsx"
+import api from "../../../config/api"
 
 export default function LoginForm() {
     const [formValues, setFormValues] = useState({
-        email: '',
-        password: '',
-    });
-    const [errorMessage, setErrorMessage] = useState('');
-    const navigate = useNavigate();
+        email: "",
+        password: "",
+    })
+    const [errorMessage, setErrorMessage] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const navigate = useNavigate()
 
     const handleInputChange = (e) => {
-        const { name, value } = e.target;
+        const { name, value } = e.target
         setFormValues((prev) => ({
             ...prev,
             [name]: value,
-        }));
-    };
-// POST
+        }))
+    }
+
     const handleLogin = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
+        setIsLoading(true)
         try {
-            const response = await api.post('/users/login', formValues); // API-Anfrage an das Backend
-            const token = response.data.token;
-            localStorage.setItem('authToken', token);
-            navigate('/dashboard');
+            const response = await api.post("/users/login", formValues)
+            const token = response.data.token
+            localStorage.setItem("authToken", token)
+
+
+            setTimeout(() => {
+                setIsLoading(false)
+                navigate("/dashboard")
+            }, 3000)
+
         } catch (error) {
-            setErrorMessage(
-                error.response?.data?.message || 'Login failed! Please try again.'
-            );
+            setIsLoading(false)
+            setErrorMessage(error.response?.data?.message || "Login failed! Please try again.")
         }
-    };
+    }
 
     return (
         <div className="login-form-full">
-            <form className="login-form" onSubmit={handleLogin}>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="E-Mail"
-                    value={formValues.email}
-                    onChange={handleInputChange}
-                    required
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={formValues.password}
-                    onChange={handleInputChange}
-                    required
-                />
-                <div className="login-button-div">
-                    <button type="submit" className="login-button">Log In
-                    </button>
-                </div>
-                {errorMessage && <p className="error-message">{errorMessage}</p>}
-            </form>
+            {isLoading ? (
+                <LoadingSpinner />
+            ) : (
+                <form className="login-form" onSubmit={handleLogin}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="E-Mail"
+                        value={formValues.email}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={formValues.password}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <div className="login-button-div">
+                        <button type="submit" className="login-button">
+                            Log In
+                        </button>
+                    </div>
+                    {errorMessage && <p className="error-message">{errorMessage}</p>}
+                </form>
+            )}
             <div className="signup-button-div">
-                <button
-                    type="button"
-                    className="signup-button"
-                    onClick={() => navigate('/register')}
-                >
+                <button type="button" className="signup-button" onClick={() => navigate("/register")}>
                     Sign Up
                 </button>
             </div>
         </div>
-    );
+    )
 }
+
