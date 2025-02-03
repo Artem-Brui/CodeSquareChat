1e809da4194eb412680b5dabdf94e0523c1b7f56
Artem-Brui <artembryj@gmail.com>
2025-02-03 13:52:15 +0100

    solved multibrauser-bug

--- frontend/src/Router/ProtectedRouter.jsx
+++ frontend/src/Router/ProtectedRouter.jsx
@@ -7,26 +7,29 @@ const ProtectedRoute = () => {
   const userId = localStorage.getItem("userId");
 
   useEffect(() => {
-    const checkAuth = async () => {
-      const checkToken = async () => {
-        try {
-          const tokenBDResponse = await fetch(
-            `http://localhost:5007/users/${userId}/token`
-          );
-          const responseData = await tokenBDResponse.json();
+    if (userId) {
+      const checkAuth = async () => {
+        const checkToken = async () => {
+          try {
+            const tokenBDResponse = await fetch(
+              `http://localhost:5007/users/${userId}/token`
+            );
+            const responseData = await tokenBDResponse.json();
 
-          setIsTokenVerifed(responseData);
-        } catch (error) {
-          console.error(error);
-        } 
+            setIsTokenVerifed(responseData);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        checkToken();
       };
 
-      checkToken();
-    };
-
-    checkAuth();
+      checkAuth();
+    } else {
+      setIsTokenVerifed(false);
+    }
   }, []);
-
+  
   if (isTokenVerifed === null) {
     return;
   }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,7 @@ import Header from '../components/layout/Header';
 import RoomsList from './RoomsList';
 
 export default function Dashboard() {
-
-    return (
+  return (
         <div className="container dashboard">
             <Header />
             <RoomsList />
