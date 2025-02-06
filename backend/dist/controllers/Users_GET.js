5dc258481ddb964c76bcea80e516fe66334e1382
Artem-Brui <artembryj@gmail.com>
2025-02-06 15:08:58 +0100

    gIgnore

--- backend/dist/controllers/Users_GET.js
+++
@@ -1,5 +0,0 @@
-import isTokenVerif from "./loginTokenVerify.js";
-export const getUsers = async (req, res) => {
-    const { token } = req.body;
-    res.status(200).json(isTokenVerif(token));
-};
--- backend/dist/controllers/errorHandler.js
+++
@@ -1,4 +0,0 @@
-const errorHandler = (res, error) => {
-    res.status(500).json({ errorMessage: error });
-};
-export default errorHandler;
